49017d292281653090127c9da231b2ddfe6f45bc
Lewis Jones <ljones140@gmail.com>
2024-07-18 13:17:25 +0100

    Clearly see partial matches

--- pkg/spdx/diff.go
+++ pkg/spdx/diff.go
@@ -2,6 +2,7 @@ package spdx
 
 import (
 	"fmt"
+	"sort"
 	"strings"
 )
 
@@ -14,7 +15,7 @@ func DiffPackages(filename string, docA, docB *Document) (string, error) {
 	for _, pkgA := range docA.Packages {
 		found := false
 		for _, pkgB := range docB.Packages {
-			if pkgA.Name == pkgB.Name {
+			if stripPrefix("/", pkgA.Name) == stripPrefix("/", pkgB.Name) {
 				found = true
 				packagesToCheck = append(packagesToCheck, packagesToCheckRelations{PackageA: pkgA, PackageB: pkgB})
 				break
@@ -33,11 +34,15 @@ func DiffPackages(filename string, docA, docB *Document) (string, error) {
 	}
 
 	for _, pkChk := range packagesToCheck {
-		diffRelations := pkChk.diffRelations()
-		if len(diffRelations) > 0 {
-			fmt.Println(builder, "Missing relations")
-			for _, rel := range diffRelations {
-				fmt.Println(builder, rel)
+		missingRelations := pkChk.diffRelations()
+		if len(missingRelations) > 0 {
+			fmt.Println(builder, "the other sbom is Missing relations:")
+			for _, rel := range missingRelations {
+				if rel.hasPartialMatchs {
+					fmt.Println(builder, "partialMatch: "+rel.purl+"->"+strings.Join(rel.partialPurls, ","))
+				} else {
+					fmt.Println(builder, "no match: "+rel.purl)
+				}
 			}
 		}
 	}
@@ -62,33 +67,82 @@ func (p *packagesToCheckRelations) Name() string {
 	return p.PackageA.Name
 }
 
-func (p *packagesToCheckRelations) diffRelations() []string {
-	missingRelations := []string{}
+type missingRelation struct {
+	purl             string
+	hasPartialMatchs bool
+	partialPurls     []string
+}
+
+func (p *packagesToCheckRelations) diffRelations() []missingRelation {
+	missingRelations := []missingRelation{}
 
 	for _, rel := range p.PackageA.Relationships {
-		_, ok := rel.Peer.(*Package)
+		partialMatches := []string{}
+		relPkg, ok := rel.Peer.(*Package)
 		if !ok {
 			continue
 		}
 		found := false
-
-		nameA := rel.Peer.(*Package).drawName(&DrawingOptions{Version: true})
+		partial := false
 		for _, relB := range p.PackageB.Relationships {
-			_, ok := relB.Peer.(*Package)
+			pkg, ok := relB.Peer.(*Package)
 			if !ok {
 				continue
 			}
-			nameB := relB.Peer.(*Package).drawName(&DrawingOptions{Version: true})
-			if nameA == nameB {
+			if relPkg.Purl().ToString() == pkg.Purl().ToString() {
 				found = true
 				break
+			}
+			// fmt.Println(found)
+			if strings.Contains(relPkg.Name, pkg.Name) {
+				partial = true
+				partialMatches = append(partialMatches, pkg.Purl().ToString())
 			}
 
 		}
 		if !found {
-			missingRelations = append(missingRelations, rel.Peer.(*Package).drawName(&DrawingOptions{Version: true, Purls: true}))
+			missingRelations = append(
+				missingRelations,
+				missingRelation{purl: rel.Peer.(*Package).Purl().ToString(), hasPartialMatchs: partial, partialPurls: partialMatches})
 		}
 
 	}
+	sort.Slice(missingRelations, func(i, j int) bool {
+		return missingRelations[i].hasPartialMatchs
+	})
+
 	return missingRelations
+}
+
+func extractName(name string) string {
+	parts := strings.Split(name, "/")
+	if len(parts) > 1 {
+		return parts[len(parts)-1]
+	}
+
+	return name
+}
+
+func stripPrefix(seperator, name string) string {
+	parts := strings.Split(name, seperator)
+	if len(parts) > 1 {
+		return parts[len(parts)-1]
+	}
+	return name
+}
+
+func stripPurlV(purl string) string {
+	parts := strings.Split(purl, "@")
+	if len(parts) > 1 {
+		s := parts[0] + strings.TrimPrefix(parts[1], "v")
+		return trimStringFromHash(s)
+	}
+	return purl
+}
+
+func trimStringFromHash(s string) string {
+	if idx := strings.Index(s, "#"); idx != -1 {
+		return s[:idx]
+	}
+	return s
 }
